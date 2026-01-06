7dd41f359d018e0548cffd651629e4ae8c7a119c
Belal Ibrahim <belal.ibrahim@proton.me>
2026-01-06 20:22:06 +0100

    c

--- init.sh
+++ init.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-DOKKU_TAG=v0.37.4
+set -e
 
-# Change this to the domain you have.
-DOMAIN=yourserver.com
+# Defaults
+DOKKU_TAG="v0.37.4"
+DATABASE_PLUGIN="mysql"
+DOMAIN=""
 
-DATABASE_PLUGIN=mysql
+while getopts "d:t:p:" opt; do
+  case "$opt" in
+    d) DOMAIN="$OPTARG" ;;
+    t) DOKKU_TAG="$OPTARG" ;;
+    p) DATABASE_PLUGIN="$OPTARG" ;;
+    *)
+      echo "Usage: $0 -d <domain> [-t dokku_tag] [-p database_plugin]"
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$DOMAIN" ]; then
+  echo "Error: domain is required"
+  echo "Usage: $0 -d <domain> [-t dokku_tag] [-p database_plugin]"
+  exit 1
+fi
+
+echo "Installing Dokku"
+echo "  Domain: $DOMAIN"
+echo "  Dokku tag: $DOKKU_TAG"
+echo "  Database plugin: $DATABASE_PLUGIN"
 
 wget -NP . https://dokku.com/install/$DOKKU_TAG/bootstrap.sh
 bash bootstrap.sh
