db84aae8c07ac00f5674dc09350dfab7198d10cf
Belal Ibrahim <belal.ibrahim@proton.me>
2026-01-20 21:17:19 +0100

    feature/gh-2-test-cert
    - Add --testcert flag, to use the ACME staging server, avoiding rate
      limits.

--- lib/letsencrypt.sh
+++ lib/letsencrypt.sh
@@ -1,7 +1,13 @@
 enable_ssl() {
   local app="$1"
   local email="$2"
+  local testcert="$3"
 
   dokku letsencrypt:set "$app" email "$email"
+
+  if [[ "$testcert" -eq "$FLAGS_TRUE" ]]; then
+    dokku letsencrypt:set "$app" server staging
+  fi
+
   dokku letsencrypt:enable "$app"
 }
