eb687ca24d939437602c3ec2e1ef398f686d8527
Brian Robert Belal <belal.ibrahim@proton.me>
2026-06-19 13:52:30 +0200

    Declare global var DOMAINS in common.sh

--- lib/common.sh
+++ lib/common.sh
@@ -2,6 +2,9 @@ log() {
   echo "==> $*"
 }
 
+# Global array populated by build_domains
+# apex domain and optionally www subdomain.
+DOMAINS=()
 build_domains() {
   local raw_domain="$1"
   local wwwsubdomain="$2"
